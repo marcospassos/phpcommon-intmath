959b88e30813f6021965910dfd7498af41a6a14b
Travis <travis@travis-ci.org>
2016-07-03 23:06:13 +0000

    Update documentation to version 1.0.0-beta

---
+++ docs/api/1.0/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","PhpCommon\\IntMath\\ArithmeticException"],["c","PhpCommon\\IntMath\\DivisionByZeroException"],["c","PhpCommon\\IntMath\\IntMath"]];
